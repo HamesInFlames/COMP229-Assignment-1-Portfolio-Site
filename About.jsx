244a59c943bc0a3ba4884e9561a9c823381d6b8b
HamesInFlames <xoxoksh05@gmail.com>
2025-05-13 15:27:15 -0400

    Initial commit

---
+++ About.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+      <>
+        <h2>About Me</h2>
+        <img src="/your-photo.jpg" alt="Your Name" />
+        <p>Hello, I am [Your Name]...</p>
+        <a href="/your-resume.pdf" download>Download My Resume</a>
+      </>
+    );
+  }
+  
